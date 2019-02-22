60a690388c0149d5bea90cf7a644f382f535b6ba
maherlaaroussi <maher@outlook.fr>
2019-02-22 14:21:05 +0100

    Some changes

--- Mapping/Sensor.py
+++ Mapping/Sensor.py
@@ -7,34 +7,36 @@ print "+-----------------------------------------------------------+"
 print "|   Mesure de distance par le capteur ultrasonore HC-SR04   |"
 print "+-----------------------------------------------------------+"
 
-Trig = 23          # Entree Trig du HC-SR04 branchee au GPIO 23
-Echo = 24         # Sortie Echo du HC-SR04 branchee au GPIO 24
-
-GPIO.setup(Trig,GPIO.OUT)
-GPIO.setup(Echo,GPIO.IN)
-
-GPIO.output(Trig, False)
-
 repet = input("Entrez un nombre de repetitions de mesure : ")
 
-for x in range(repet):    # On prend la mesure "repet" fois
-
-   time.sleep(1)       # On la prend toute les 1 seconde
-
-   GPIO.output(Trig, True)
-   time.sleep(0.00001)
-   GPIO.output(Trig, False)
-
-   while GPIO.input(Echo)==0:  ## Emission de l'ultrason
-     debutImpulsion = time.time()
-
-   while GPIO.input(Echo)==1:   ## Retour de l'Echo
-     finImpulsion = time.time()
-
-   distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
-
-   print "La distance est de : ", distance," cm"
-   print "Debut : ", debutImpulsion," ms"
-   print "Fin : ", finImpulsion," ms"
+for x in range(repet) :
+   sensor(23, 24)
 
 GPIO.cleanup()
+
+def sensor(t, e) :
+
+    GPIO.setup(Trig,GPIO.OUT)
+    GPIO.setup(Echo,GPIO.IN)
+
+    GPIO.output(Trig, False)
+
+     time.sleep(1)
+
+     GPIO.output(t, True)
+     time.sleep(0.00001)
+     GPIO.output(t, False)
+
+     while GPIO.input(e)==0:
+       debutImpulsion = time.time()
+
+     while GPIO.input(e)==1:
+       finImpulsion = time.time()
+
+     distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)
+     temps = finImpulsion - debutImpulsion
+
+     print "La distance est de : ", distance," cm"
+     print "Debut : ", temps," ms"
+
+     GPIO.cleanup()
