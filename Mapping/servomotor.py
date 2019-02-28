702cb60c3bc7781519b0f1f3061122993ba6819e
maherlaaroussi <maher@outlook.fr>
2019-02-28 13:27:50 +0100

    Tests

--- Mapping/servomotor.py
+++ Mapping/servomotor.py
@@ -6,11 +6,11 @@ def rotate(data, angle) :
     GPIO.setup(data, GPIO.OUT)
     pwm = GPIO.PWM(data,100)
 
-    ajoutAngle = 5
-    angle = float(angle)
+    addAngle = 5
+    ang = float(angle)
 
     pwm.start(5)
 
-    angleChoisi = angle/10 + ajoutAngle
+    angleChoisi = ang/10 + addAngle
     pwm.ChangeDutyCycle(angleChoisi)
     time.sleep(10)
