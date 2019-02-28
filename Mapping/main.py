9d3e678983e2bc5d896a09ae53422c77988832d7
maherlaaroussi <maher@outlook.fr>
2019-02-28 13:10:29 +0100

    Some changes

--- Mapping/main.py
+++ Mapping/main.py
@@ -6,21 +6,17 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # GPIOs sensors
-t1 = 23
-e1 = 24
-t2 = 21
-e2 = 20
+t1 = 21
+e1 = 20
+t2 = 26
+e2 = 19
 
 # =========================================================
 
-trigger = input("GPIO Trigger : ")
-echo = input("GPIO Echo : ")
-
-servo = input("GPIO Servo : ")
-
 for x in range(5) :
-   scan(trigger, echo)
-   rotate(servo, 180)
+   scan(t1, e1)
+   scan(t2, e2)
+   rotate(4, 180)
 
 # Clean GPIOs
 GPIO.cleanup()
