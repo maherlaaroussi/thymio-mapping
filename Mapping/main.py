6d0663f22905376d4e6c9e6d8e9b40d05cd941f2
maherlaaroussi <maher@outlook.fr>
2019-03-09 16:08:26 +0100

    Update main.py

--- Mapping/main.py
+++ Mapping/main.py
@@ -1,10 +1,14 @@
 from scan import *
+from fonctionDeplacement import *
 
 # Mode GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+
+
 scan("fast")
+avancerTemps(1)
 
 # Clean GPIOs
 GPIO.cleanup()
