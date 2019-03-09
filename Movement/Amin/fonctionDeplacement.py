e04a69c58dea3b80cae6c178ed132d6c787dc049
maherlaaroussi <maher@outlook.fr>
2019-03-09 16:10:06 +0100

    Fix error

--- Movement/Amin/fonctionDeplacement.py
+++ Movement/Amin/fonctionDeplacement.py
@@ -26,9 +26,9 @@ rotationRobot=0
 """
 Fonction de calcul
 """
-def obstacle(distance1,distance2,distance3,distance4,distance5,distance6): # sert à rien 
+def obstacle(distance1,distance2,distance3,distance4,distance5,distance6): # sert à rien
     if r.prox_horizontal[0] > distance1 or r.prox_horizontal[1] > distance2 or r.prox_horizontal[2] > distance3 or r.prox_horizontal[3] > distance4 or r.prox_horizontal[4] > distance5 or r.prox_horizontal[5] > distance6:
-        r.sound_system(3)   
+        r.sound_system(3)
         #r.sound_freq()
         return 1
     else:
@@ -41,57 +41,57 @@ def avancerTemps(temps):
 
 def calculTempsAvancement(distance):
     global tempsAvancer
-    
+
     temps = distance/tempsAvancer
     #print("-Avec une distance de "+str(distance)+" cm, le robot a avancé pendant "+str(temps)+"s.")
     time.sleep((temps))
-   
+
 
 def calculTempsRotation(degree):
     global tempsRotation
-    
+
     rotation = degree*tempsRotation
     #print("-Avec une rotation de "+str(degree)+" degree(s), le robot tournera  pendant "+str(rotation)+"s.")
     time.sleep(rotation)
-    
+
 def distanceParcourue(distance):
     global rotationRobot
     global x
     global y
-    
+
     if rotationRobot == 90:
         x = x + distance
-        
+
     if rotationRobot == 270:
         x = x-distance
-      
+
     if rotationRobot == 180:
         y = y- distance
-    
+
     if rotationRobot == 0:
         y = y + distance
-    
+
     if rotationRobot == 360:
         y = y+distance
-    
+
     if rotationRobot >270 and rotationRobot < 360:
         tmp1 = m.cos(360-rotationRobot) * distance
         tmp2 = m.sin(360-rotationRobot) * distance
         x = x-tmp1
         y = y+tmp2
-        
+
     if rotationRobot >0 and rotationRobot <90:
         tmp1 = m.cos(90-rotationRobot) * distance
         tmp2 = m.sin(90-rotationRobot) * distance
         x = x+tmp1
         y = y+tmp2
-        
+
     if rotationRobot >90 and rotationRobot < 180:
         tmp1 = m.cos(rotationRobot-90) * distance
         tmp2 = m.sin(rotationRobot-90) * distance
         x = x+tmp1
         y = y - tmp2
-        
+
     if rotationRobot >180 and rotationRobot <270:
         tmp1 = m.cos(270-rotationRobot) * distance
         tmp2 = m.sin(270-rotationRobot) * distance
@@ -109,9 +109,9 @@ def incrementerRotationRobot(degree):
         tmp = 360 +tmp
 
     rotationRobot = tmp
-    
 
-    
+
+
 ################################################################################################################s
 """
 Fonc#tion de mouvement
@@ -131,7 +131,7 @@ def reculer(distance):
 
 def stop():
     r.wheels(0,0)
- 
+
 def attend(secondes):
     time.sleep(secondes)
 
@@ -162,7 +162,7 @@ def allerA(cordX,cordY):
     tmp = rotationRobot
     if cordY > y : #on va en haut
         if cordX > x : #On va en haut à droite
-            
+
             coteAdjacent = abs(cordX - x)
             coteOppose   = abs(cordY - y)
             hypotenus    = m.hypot(coteAdjacent,coteOppose)
@@ -173,9 +173,9 @@ def allerA(cordX,cordY):
             changerRotation(tmp)
             x = cordX
             y = cordY
-            
+
         if cordX < x: #On va en haut à gauche
-            
+
             coteAdjacent = abs(x - cordX)
             coteOppose   = abs(cordY - y)
             hypotenus    = m.hypot(coteAdjacent,coteOppose)
@@ -186,10 +186,10 @@ def allerA(cordX,cordY):
             changerRotation(tmp)
             x = cordX
             y = cordY
-            
+
     if cordY < y: #On va en bas
         if cordX > x: #On va en bas à droite
-            
+
             coteAdjacent = abs(cordX - x)
             coteOppose   = abs(y - cordY)
             hypotenus    = m.hypot(coteAdjacent,coteOppose)
@@ -200,9 +200,8 @@ def allerA(cordX,cordY):
             changerRotation(tmp)
             x = cordX
             y = cordY
-            
+
         if cordX < x: #On va en bas à gauche
-            $
             coteAdjacent = abs(x - cordX)
             coteOppose   = abs(cordY - y)
             hypotenus    = m.hypot(coteAdjacent,coteOppose)
@@ -227,7 +226,7 @@ if __name__ == "__main__":
     print()
     print("###########################################################\n")
     print("Bonjour ! Il est temps de vous montrer ma vraie puissance !\n")
-    print("###########################################################\n") 
+    print("###########################################################\n")
     print("-Initialisation : x vaut : "+str(x)+" y vaut : "+str(y)+" et ma rotation actuelle est :"+str(rotationRobot)+" degrée(s)")
     print("-Commençons soft : j'avance de 5 centimètres !\n")
     attend(1)
@@ -246,5 +245,5 @@ if __name__ == "__main__":
     print("-Mes valeurs : x vaut : "+str(x)+" y vaut : "+str(y)+" et ma rotation actuelle est :"+str(rotationRobot)+" degrée(s)")
     print("-Fin de mon petit voyage, ai-je réussi ?")
     attend(1)
-    
+
     r.quit()
