d139191fbc1c5eaec5e0b4c506066ce9923bd049
maherlaaroussi <maher@outlook.fr>
2019-03-21 22:57:26 +0100

    Avancement

--- src/image.php
+++ src/image.php
@@ -1,15 +1,15 @@
 <?php
 
   header( "Content-type: image/png" );
-  
-  $myImage = imagecreate( 200, 100 );
 
-  $myGray = imagecolorallocate( $myImage, 204, 204, 204 );
-  $myBlack = imagecolorallocate( $myImage, 0, 0, 0 );
-  imageline( $myImage, 15, 35, 120, 60, $myBlack );
+  $myImage = imagecreate(600, 600);
 
-  imagepng( $myImage );
+  $myWhite = imagecolorallocate($myImage, 255, 255, 255);
+  $myBlack = imagecolorallocate($myImage, 0, 0, 0);
 
+  imageline($myImage, 15, 35, 120, 60, $myBlack);
+
+  imagepng($myImage);
   imagedestroy( $myImage );
 
 ?>
