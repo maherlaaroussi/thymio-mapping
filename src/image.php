00caac5135641763ef7bfa4f75e11688060e1f5e
maherlaaroussi <maher@outlook.fr>
2019-03-21 18:10:31 +0100

    Avancement

--- src/image.php
+++ src/image.php
@@ -1,18 +1,11 @@
 <?php
 
-header ("Content-type: image/png");
-
-
-$im = @ImageCreate (100, 200)
-or die ("Cannot Initialize new GD image stream");
-
-$background_color = ImageColorAllocate ($im, 224, 234, 234);
-
-$text_color = ImageColorAllocate ($im, 233, 14, 91);
-
- imageline ($im, 0, 0, 100, 200, $text_color);
- imageline ($im, 100, 0, 0, 200, $text_color);
-
-ImagePng ($im);
+  $myImage = imagecreate( 200, 100 );
+  $myGray = imagecolorallocate( $myImage, 204, 204, 204 );
+  $myBlack = imagecolorallocate( $myImage, 0, 0, 0 );
+  imageline( $myImage, 15, 35, 120, 60, $myBlack );
+  header( "Content-type: image/png" );
+  imagepng( $myImage );
+  imagedestroy( $myImage );
 
 ?>
