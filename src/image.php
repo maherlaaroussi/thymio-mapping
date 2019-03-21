fa0896bd6f342d2a5bd89ad9c9fdda98ef3be740
maherlaaroussi <maher@outlook.fr>
2019-03-21 22:59:44 +0100

    Avancement

--- src/image.php
+++ src/image.php
@@ -8,7 +8,7 @@
   $myBlack = imagecolorallocate($myImage, 0, 0, 0);
   $myWhite = imagecolorallocate($myImage, 255, 255, 255);
 
-  imageline($myImage, 15, 35, 120, 60, $myBlack);
+  imageline($myImage, 0, 0, 50, 50, $myWhite);
 
   imagepng($myImage);
   imagedestroy( $myImage );
