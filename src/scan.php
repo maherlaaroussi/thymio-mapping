51667c67f20045fabb867fe790716fc90ee72962
maherlaaroussi <maher@outlook.fr>
2019-03-22 09:27:25 +0100

    Read values file

--- src/scan.php
+++ src/scan.php
@@ -1,15 +1,32 @@
 <?php
 
   $filename = $_GET['filename'];
+  $size = 600;
+  $original_x = $size / 2;
+  $original_y = $size / 2;
 
   $output = exec("sudo /usr/bin/python3 /var/www/html/python/main.py " .  $filename);
 
-  $myImage = imagecreate(600, 600);
+  $myImage = imagecreate($size, $size);
 
   $myBlack = imagecolorallocate($myImage, 0, 0, 0);
   $myWhite = imagecolorallocate($myImage, 255, 255, 255);
 
-  imageline($myImage, 0, 0, 500, 500, $myWhite);
+  /* Read values */
+  $handle = fopen("data/" . filename . ".txt", "r");
+  $values = "";
+
+  if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+      $values = explode(":", $line);
+      $x = $values[1] * cos(($values[0] + 180) / 180. * pi())
+      $y = $values[2] * sin(($values[0] + 180) / 180. * pi())
+      imageline($myImage, $original_x, $original_y, $x, $y, $myWhite);
+    }
+  fclose($handle);
+
+  } else {
+  }
 
   header( "Content-type: image/png" );
   imagepng($myImage, $filename . ".png");
