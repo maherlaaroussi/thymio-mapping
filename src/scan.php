a1c2e65481f75fa8eb38f2160697f06902562637
maherlaaroussi <maher@outlook.fr>
2019-03-22 09:51:45 +0100

    Avancement

--- src/scan.php
+++ src/scan.php
@@ -12,17 +12,23 @@
   $myBlack = imagecolorallocate($myImage, 0, 0, 0);
   $myWhite = imagecolorallocate($myImage, 255, 255, 255);
 
+  $output = explode(",", $output);
+  echo $output;
+
+  /*
   $handle = fopen("data/" . filename . ".txt", "r");
   $values = "";
 
   if ($handle) {
+
     while (($line = fgets($handle)) !== false) {
       $values = explode(":", $line);
       $posX = 100; // $values[1] * cos($values[0] / 180 * pi())
       $posY = 200; // $values[2] * sin($values[0] / 180 * pi())
       imageline($myImage, $original_x, $original_y, $posX, $posY, $myWhite);
     }
-  fclose($handle);
+
+    fclose($handle);
 
   } else {
       // imageline($myImage, $original_x, $original_y, 200, 200, $myWhite);
@@ -33,6 +39,8 @@
   header("Content-disposition: attachment; filename=" . $filename . ".png");
   header('Content-Description: File Transfer');
   readfile("" . $filename . ".png");
+
+  */
 
   imagedestroy($myImage);
 
