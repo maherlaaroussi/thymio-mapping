253cd5b3eb5376d1af5249bd6f73800338075eb4
maherlaaroussi <maher@outlook.fr>
2019-03-22 10:26:05 +0100

    Avancement

--- src/scan.php
+++ src/scan.php
@@ -15,13 +15,15 @@
   $output = json_decode($output);
 
   foreach ($output as $value) {
+
     $posXF = $value[1] * cos($value[0] / 180 * pi());
     $posYF = $value[1] * sin($value[0] / 180 * pi());
     imageline($myImage, $original_x, $original_y, $posXF, $posYF, $myWhite);
 
-    $posXB = $value[2] * cos(()$value[0] + 180) / 180 * pi());
+    $posXB = $value[2] * cos(($value[0] + 180) / 180 * pi());
     $posYB = $value[2] * sin(($value[0] + 180) / 180 * pi());
     imageline($myImage, $original_x, $original_y, $posXB, $posYB, $myWhite);
+
   }
 
   header( "Content-type: image/png" );
