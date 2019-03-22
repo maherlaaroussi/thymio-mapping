bcd75c41e0832bee975323ef8747f5c7e1650a60
maherlaaroussi <maher@outlook.fr>
2019-03-22 10:59:53 +0100

    Avancement

--- src/scan.php
+++ src/scan.php
@@ -4,7 +4,7 @@
   $size = 600;
   $original_x = $size / 2;
   $original_y = $size / 2;
-  $resize = 2;
+  $resize = 5;
 
   $output = exec("sudo /usr/bin/python3 /var/www/html/python/main.py " .  $filename);
 
